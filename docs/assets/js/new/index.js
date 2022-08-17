e1f5abe3c9bec863c63ecb04fe71fe0aa92e59ed
Vlad.Bondar <vlad.bondar@moyo.ua>
2022-08-17 13:55:19 +0300

    Fixes

--- docs/assets/js/new/index.js
+++ docs/assets/js/new/index.js
@@ -31,47 +31,163 @@ jQuery('.why-us__title-item').click(e => {
         jQuery('.why-us__content-item').hide();
         jQuery(`#why-us-${hash}`).show();
     }
-})
+});
 // End why us tabs
 
+// cursor function
 window.onload = function () {
-    const images = document.querySelectorAll('.with-custom-scroll');
-    if (window.innerWidth <= 768 || !images.length) return;
+
+    const { gsap, CircleType } = window;
+    const cursorWrapper = document.querySelector(".cursor");
+
+    if(!cursorWrapper || window.innerWidth <= 768) return;
+
+    const cursorOuter = document.querySelector(".cursor--large");
+    const cursorInner = document.querySelector(".cursor--small");
+    const cursorTextContainerEl = document.querySelector(".cursor--text");
+    const cursorTextEl = cursorTextContainerEl.querySelector(".text");
+
+    const hoverItems = document.querySelectorAll(".with-custom-scroll");
+    const hoverEffectDuration = 0.3;
+    let isHovered = false;
+    let initialCursorHeight;
+
+    const cursorRotationDuration = 8;
+
+    let circleType = new CircleType(cursorTextEl);
+    circleType.radius(50);
+
+    setTimeout(() => {
+        initialCursorHeight = circleType.container.style.getPropertyValue("height");
+        console.log(initialCursorHeight);
+    }, 150);
+
+    hoverItems.forEach((item) => {
+        item.addEventListener("pointerenter", () => {
+            cursorWrapper.style.opacity = 1;
+        });
+        item.addEventListener("pointerleave", () => {
+            cursorWrapper.style.opacity = 0;
+        });
+        item.addEventListener("pointerenter", handlePointerEnter);
+        item.addEventListener("pointerleave", handlePointerLeave);
+    });
+
     let mouse = {
         x: -100,
         y: -100
     };
 
     document.body.addEventListener("pointermove", updateCursorPosition);
-    const cursor = document.querySelector('.with-custom-scroll-inner');
 
-    new CircleType(cursor).radius(44);
     function updateCursorPosition(e) {
         mouse.x = e.pageX;
         mouse.y = e.pageY;
     }
 
     function updateCursor() {
-        cursor.style.left = mouse.x + 'px';
-        cursor.style.top = mouse.y + 'px';
+        gsap.set([cursorInner, cursorTextContainerEl], {
+            x: mouse.x,
+            y: mouse.y
+        });
+
+        gsap.to(cursorOuter, {
+            duration: 0.15,
+            x: mouse.x,
+            y: mouse.y
+        });
+
+        if (!isHovered) {
+            gsap.to(cursorTextContainerEl, hoverEffectDuration * 0.5, {
+                opacity: 0
+            });
+            gsap.set(cursorTextContainerEl, {
+                rotate: 0
+            });
+        }
+
+        requestAnimationFrame(updateCursor);
     }
 
-    function showCursor() {
-        cursor.classList.remove('hidden');
+    updateCursor();
+
+    function handlePointerEnter(e) {
+        isHovered = true;
+
+        const target = e.currentTarget;
+        updateCursorText(target);
+
+        gsap.set([cursorTextContainerEl, cursorTextEl], {
+            height: initialCursorHeight,
+            width: initialCursorHeight
+        });
+
+        gsap.fromTo(
+            cursorTextContainerEl,
+            {
+                rotate: 0
+            },
+            {
+                duration: cursorRotationDuration,
+                rotate: 360,
+                ease: "none",
+                repeat: -1
+            }
+        );
+
+        gsap.to(cursorInner, hoverEffectDuration, {
+            scale: 2
+        });
+
+        gsap.fromTo(
+            cursorTextContainerEl,
+            hoverEffectDuration,
+            {
+                scale: 1.2,
+                opacity: 0
+            },
+            {
+                delay: hoverEffectDuration * 0.75,
+                scale: 1,
+                opacity: 1
+            }
+        );
+        gsap.to(cursorOuter, hoverEffectDuration, {
+            scale: 1.2,
+            opacity: 0
+        });
     }
 
-    function removeCursor() {
-        cursor.classList.add('hidden');
+    function handlePointerLeave() {
+        isHovered = false;
+        gsap.to([cursorInner, cursorOuter], hoverEffectDuration, {
+            scale: 1,
+            opacity: 1
+        });
     }
 
-    images.forEach(function (item) {
-        item.setAttribute('data-left', 0);
-        item.setAttribute('data-top', 0);
-        item.addEventListener('pointermove', () => requestAnimationFrame(updateCursor));
-        item.addEventListener('pointermove', showCursor);
-        item.addEventListener('pointerleave', removeCursor);
-    });
-}
+    function updateCursorText(textEl) {
+        const cursorTextRepeatTimes = textEl.getAttribute("data-cursor-text-repeat");
+        const cursorText = returnMultipleString(
+            textEl.getAttribute("data-cursor-text"),
+            cursorTextRepeatTimes
+        );
+
+        circleType.destroy();
+
+        cursorTextEl.innerHTML = cursorText;
+        circleType = new CircleType(cursorTextEl);
+    }
+
+    function returnMultipleString(string, count) {
+        let s = "";
+        for (let i = 0; i < count; i++) {
+            s += ` ${string} `;
+        }
+        return s;
+    }
+};
+// cursor function END
 
 
 jQuery(function () {
